7d097d467eac8c8b0b3b23315267fb702e5a654a
Mitsuki ESU <e165749@ie.u-ryukyu.ac.jp>
2016-12-13 02:41:37 +0900

    report5

---
+++ src/Main/jp/ac/uryukyu/ie/e165749/Main.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e165749;
+
+/**
+ * Created by South on 2016/12/13.
+ */
+public class Main {
+    public static void main (String[] args) {
+        String str = null;
+        int len = str.length();
+        System.out.println(len);
+    }
+}
