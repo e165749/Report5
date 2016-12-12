03680a1aa8994c62851df33dbaff5bdab966291e
Mitsuki ESU <e165749@ie.u-ryukyu.ac.jp>
2016-12-13 04:23:58 +0900

    report5

--- src/Main/jp/ac/uryukyu/ie/e165749/Main.java
+++ src/Main/jp/ac/uryukyu/ie/e165749/Main.java
@@ -5,8 +5,17 @@ package jp.ac.uryukyu.ie.e165749;
  */
 public class Main {
     public static void main (String[] args) {
+
         String str = null;
-        int len = str.length();
-        System.out.println(len);
+
+        try{
+            int len = str.length();
+            System.out.println(len);
+        }
+
+        catch(java.lang.NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            e.printStackTrace();
+        }
     }
 }
